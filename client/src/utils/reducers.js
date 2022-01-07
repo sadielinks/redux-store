ac9108d44c35b8d0303f9626221a1f0b18468518
Sadie Sial <sadiesial@gmail.com>
2022-01-07 13:46:21 -0600

    created initialState fx

--- client/src/utils/reducers.js
+++ client/src/utils/reducers.js
@@ -1,4 +1,5 @@
-import { useReducer } from 'react';
+// remove import since not used anymore
+// import { useReducer } from 'react';
 import {
   UPDATE_PRODUCTS,
   ADD_TO_CART,
@@ -11,8 +12,16 @@ import {
   TOGGLE_CART,
 } from './actions';
 
+const initialState = {
+  products: [],
+  cart: [],
+  cartOpen: false,
+  categories: [],
+  currentCategory: '',
+}
+
 // The reducer is a function that accepts the current state and an action. It returns a new state based on that action.
-export const reducer = (state, action) => {
+export const reducer = (state = initialState, action) => {
   switch (action.type) {
     // Returns a copy of state with an update products array. We use the action.products property and spread it's contents into the new array.
     case UPDATE_PRODUCTS:
