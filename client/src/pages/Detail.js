edd1a4ac84cb3719ec75bdc6de2c0e148d77cc07
Sadie Sial <sadiesial@gmail.com>
2022-01-09 14:15:48 -0600

    removed GlobalState for redux

--- client/src/pages/Detail.js
+++ client/src/pages/Detail.js
@@ -1,9 +1,9 @@
 import React, { useEffect, useState } from 'react';
 import { Link, useParams } from 'react-router-dom';
 import { useQuery } from '@apollo/client';
-
 import Cart from '../components/Cart';
-import { useStoreContext } from '../utils/GlobalState';
+// removed GlobalState import for redux
+import { useSelector, useDispatch } from 'react-redux';
 import {
   REMOVE_FROM_CART,
   UPDATE_CART_QUANTITY,
@@ -15,7 +15,11 @@ import { idbPromise } from '../utils/helpers';
 import spinner from '../assets/spinner.gif';
 
 function Detail() {
-  const [state, dispatch] = useStoreContext();
+  // removed useStoreContext for redux
+  const state = useSelector(state => state);
+  const dispatch = useDispatch();
+
+  // window.location will now provide id
   const { id } = useParams();
 
   const [currentProduct, setCurrentProduct] = useState({});
